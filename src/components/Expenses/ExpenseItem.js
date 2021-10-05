4dbc0d98fce572d43a72820cc514cb2ae4ea8911
Amina Alic <amina.alic@visionect.com>
2021-10-05 11:09:55 +0200

    Working with state and event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, amount, title }) => {
-  const clickHandler = () => {
-    console.log("clicked");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -14,7 +11,6 @@ const ExpenseItem = ({ date, amount, title }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
