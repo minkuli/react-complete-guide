0222516231792dafd19c620947bda516de2bcf68
Amina Alic <amina.alic@visionect.com>
2021-10-05 17:33:20 +0200

    Module 5

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,42 +1,29 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
-  const [selectedYear, setSelectedYear] = useState("2021");
+  const [selectedYear, setSelectedYear] = useState("2020");
   const expensesFilterHandler = (year) => {
     setSelectedYear(year);
   };
 
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear() === parseInt(selectedYear);
+  });
+
   return (
     <div>
       <Card className="expenses">
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesFilter
           onFilterExpenses={expensesFilterHandler}
           selected={selectedYear}
         />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
